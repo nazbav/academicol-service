3e656cea710802a42576fe7f2546146964b3111d
Nazym Bavbekov <joker2000joker@list.ru>
2020-02-01 22:55:06 +0400

    ops

--- sql.sql
+++ sql.sql
@@ -1,66 +1,40 @@
 CREATE TABLE IF NOT EXISTS `peers` (
-  `peer_id` int(11) NOT NULL COMMENT 'Код получателя',
-  `tag` varchar(255) NOT NULL COMMENT 'Тег для поиска',
-  `type` int(1) NOT NULL DEFAULT '1' COMMENT '1 -- VK, 0 -- Никуда'
+  `peer_id` int(11) NOT NULL COMMENT 'ГЉГ®Г¤ ГЇГ®Г«ГіГ·Г ГІГҐГ«Гї',
+  `tag` varchar(255) NOT NULL COMMENT 'Г’ГҐГЈ Г¤Г«Гї ГЇГ®ГЁГ±ГЄГ ',
+  `type` int(1) NOT NULL DEFAULT '1' COMMENT '1 -- VK, 0 -- ГЌГЁГЄГіГ¤Г '
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `replaces` (
   `id` int(11) NOT NULL,
-  `replace_file` int(11) NOT NULL COMMENT 'Файл замен',
-  `tag` varchar(255) NOT NULL COMMENT 'Тег для поиска'
+  `replace_file` int(11) NOT NULL COMMENT 'Г”Г Г©Г« Г§Г Г¬ГҐГ­',
+  `tag` varchar(255) NOT NULL COMMENT 'Г’ГҐГЈ Г¤Г«Гї ГЇГ®ГЁГ±ГЄГ '
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` int(11) NOT NULL COMMENT 'Код задачи',
-  `peer_id` int(11) NOT NULL COMMENT 'Код получателя',
-  `replace_id` int(11) NOT NULL COMMENT 'Код файла замен'
+  `id` int(11) NOT NULL COMMENT 'ГЉГ®Г¤ Г§Г Г¤Г Г·ГЁ',
+  `peer_id` int(11) NOT NULL COMMENT 'ГЉГ®Г¤ ГЇГ®Г«ГіГ·Г ГІГҐГ«Гї',
+  `replace_id` int(11) NOT NULL COMMENT 'ГЉГ®Г¤ ГґГ Г©Г«Г  Г§Г Г¬ГҐГ­'
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `replaces_files` (
-  `id` int(11) NOT NULL COMMENT 'Код файла замен',
-  `name` varchar(255) NOT NULL COMMENT 'Имя файла замен',
-  `date` int(11) NOT NULL COMMENT 'Дата вступления замены',
-  `url` varchar(255) NOT NULL COMMENT 'Адрес файла замен'
+  `id` int(11) NOT NULL COMMENT 'ГЉГ®Г¤ ГґГ Г©Г«Г  Г§Г Г¬ГҐГ­',
+  `name` varchar(255) NOT NULL COMMENT 'Г€Г¬Гї ГґГ Г©Г«Г  Г§Г Г¬ГҐГ­',
+  `date` int(11) NOT NULL COMMENT 'Г„Г ГІГ  ГўГ±ГІГіГЇГ«ГҐГ­ГЁГї Г§Г Г¬ГҐГ­Г»',
+  `url` varchar(255) NOT NULL COMMENT 'ГЂГ¤Г°ГҐГ± ГґГ Г©Г«Г  Г§Г Г¬ГҐГ­'
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO `peers` (`peer_id`, `tag`, `type`) VALUES
-(153143942, '2ПКС-11,3ПКС-9', 1),
-(157410260, '2ПКС-11,3ПКС-9', 1),
-(211984675, '2ЗИО-11,3ЗИО-9', 1),
-(452444965, '2ПКС-11,3ПКС-9', 1);
-
-INSERT INTO `replaces` (`id`, `replace_file`, `tag`) VALUES
-(1, 5, 'Пучкин А.В.'),
-(2, 5, '1ЗИО-9'),
-(3, 5, '1ПСО-9'),
-(4, 5, '1ПД-9'),
-(5, 5, '1ТОП-9'),
-(6, 5, '1К-9'),
-(7, 5, '1Б-9,1БД-9'),
-(8, 5, '2ЗИО-11,3ЗИО-9'),
-(9, 5, '3ТОП-11,4ТОП-9'),
-(10, 6, 'Зданович Г.В.'),
-(11, 6, '1ПД-11,2ПД-9'),
-(12, 6, '3ТОП-11,4ТОП-9');
-
-INSERT INTO `replaces_files` (`id`, `name`, `date`, `url`) VALUES
-(1, '20 января 2020', 1579550340, 'https://vk.cc/ag7OCK'),
-(2, '18 января 2020', 1579377540, 'https://vk.cc/ag7ODO');
-
-
-
 --
--- Индексы сохранённых таблиц
+-- Г€Г­Г¤ГҐГЄГ±Г» Г±Г®ГµГ°Г Г­ВёГ­Г­Г»Гµ ГІГ ГЎГ«ГЁГ¶
 --
 
 --
--- Индексы таблицы `peers`
+-- Г€Г­Г¤ГҐГЄГ±Г» ГІГ ГЎГ«ГЁГ¶Г» `peers`
 --
 ALTER TABLE `peers`
   ADD PRIMARY KEY (`peer_id`);
 
 --
--- Индексы таблицы `replaces`
+-- Г€Г­Г¤ГҐГЄГ±Г» ГІГ ГЎГ«ГЁГ¶Г» `replaces`
 --
 ALTER TABLE `replaces`
   ADD PRIMARY KEY (`id`),
@@ -68,7 +42,7 @@ ALTER TABLE `replaces`
   ADD KEY `replace_file` (`replace_file`);
 
 --
--- Индексы таблицы `replaces_files`
+-- Г€Г­Г¤ГҐГЄГ±Г» ГІГ ГЎГ«ГЁГ¶Г» `replaces_files`
 --
 ALTER TABLE `replaces_files`
   ADD PRIMARY KEY (`id`),
@@ -78,7 +52,7 @@ ALTER TABLE `replaces_files`
   ADD KEY `id_2` (`id`);
 
 --
--- Индексы таблицы `tasks`
+-- Г€Г­Г¤ГҐГЄГ±Г» ГІГ ГЎГ«ГЁГ¶Г» `tasks`
 --
 ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`),
@@ -86,36 +60,36 @@ ALTER TABLE `tasks`
   ADD KEY `replace_id` (`replace_id`);
 
 --
--- AUTO_INCREMENT для сохранённых таблиц
+-- AUTO_INCREMENT Г¤Г«Гї Г±Г®ГµГ°Г Г­ВёГ­Г­Г»Гµ ГІГ ГЎГ«ГЁГ¶
 --
 
 --
--- AUTO_INCREMENT для таблицы `replaces`
+-- AUTO_INCREMENT Г¤Г«Гї ГІГ ГЎГ«ГЁГ¶Г» `replaces`
 --
 ALTER TABLE `replaces`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT для таблицы `replaces_files`
+-- AUTO_INCREMENT Г¤Г«Гї ГІГ ГЎГ«ГЁГ¶Г» `replaces_files`
 --
 ALTER TABLE `replaces_files`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT для таблицы `tasks`
+-- AUTO_INCREMENT Г¤Г«Гї ГІГ ГЎГ«ГЁГ¶Г» `tasks`
 --
 ALTER TABLE `tasks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- Ограничения внешнего ключа сохраненных таблиц
+-- ГЋГЈГ°Г Г­ГЁГ·ГҐГ­ГЁГї ГўГ­ГҐГёГ­ГҐГЈГ® ГЄГ«ГѕГ·Г  Г±Г®ГµГ°Г Г­ГҐГ­Г­Г»Гµ ГІГ ГЎГ«ГЁГ¶
 --
 
 --
--- Ограничения внешнего ключа таблицы `replaces`
+-- ГЋГЈГ°Г Г­ГЁГ·ГҐГ­ГЁГї ГўГ­ГҐГёГ­ГҐГЈГ® ГЄГ«ГѕГ·Г  ГІГ ГЎГ«ГЁГ¶Г» `replaces`
 --
 ALTER TABLE `replaces`
   ADD CONSTRAINT `replaces_ibfk_1` FOREIGN KEY (`replace_file`) REFERENCES `replaces_files` (`id`);
 
 --
--- Ограничения внешнего ключа таблицы `tasks`
+-- ГЋГЈГ°Г Г­ГЁГ·ГҐГ­ГЁГї ГўГ­ГҐГёГ­ГҐГЈГ® ГЄГ«ГѕГ·Г  ГІГ ГЎГ«ГЁГ¶Г» `tasks`
 --
 ALTER TABLE `tasks`
   ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`replace_id`) REFERENCES `replaces_files` (`id`),
